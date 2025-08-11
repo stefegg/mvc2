267bb84b8ad7b225365b8e3948a3aa8c2ad70dcf
Stef Egbert <stephenegbert@gmail.com>
2025-08-11 10:26:38 -0400

    cleanup fighter card

--- components/FighterSelectCard/index.tsx
+++ components/FighterSelectCard/index.tsx
@@ -34,7 +34,7 @@ const FighterSelectCard = ({
   return (
     <div
       key={fighter.id}
-      className={`${getBgColor()} p-4 rounded-lg flex flex-col items-center`}
+      className={`${getBgColor()} p-4 rounded-lg flex flex-col items-center relative`}
     >
       <Image
         src={`${fighter.portrait}`}
@@ -43,26 +43,35 @@ const FighterSelectCard = ({
         height={500}
         style={{ width: "100%", height: "auto", borderRadius: "8px" }}
       />
-      <p className="text-neo-yellow">{fighter.name}</p>
-      <p className="text-neo-purple">{fighter.stats.type}</p>
-      <div className="text-sm text-neo-blue flex flex-row gap-2">
-        <div>Logic: {fighter.stats.logic}</div>
-        <div>Flow: {fighter.stats.flow}</div>
-        <div>Chaos: {fighter.stats.chaos}</div>
-      </div>
-      <div className="flex flex-row">
-        {!isInTeamOne && !teamOneComplete && (
-          <Button
-            text="Add to Team One"
-            onClick={() => onAddToTeam(fighter, 1)}
-          />
-        )}
-        {!isInTeamTwo && !teamTwoComplete && (
-          <Button
-            text="Add to Team Two"
-            onClick={() => onAddToTeam(fighter, 2)}
-          />
-        )}
+      <div className="absolute bottom-0 bg-black/80 flex flex-col w-full">
+        <div className="flex flex-row gap-4 justify-center">
+          <p className="text-neo-yellow">{fighter.name}</p>
+          <p className="text-neo-purple">{fighter.stats.type}</p>
+        </div>
+        <div className="flex flex-row justify-center">
+          <p className="text-neo-red text-sm">{fighter.origin}</p>
+        </div>
+        <div className="text-sm text-neo-blue flex flex-row gap-2 justify-center pb-2">
+          <div>Logic: {fighter.stats.logic}</div>
+          <div>Flow: {fighter.stats.flow}</div>
+          <div>Chaos: {fighter.stats.chaos}</div>
+        </div>
+        <div className="flex flex-row justify-evenly">
+          {!isInTeamOne && !teamOneComplete && (
+            <Button
+              text="+ Team One"
+              onClick={() => onAddToTeam(fighter, 1)}
+              className="hover:text-neo-yellow p-0 border border-neo-white hover:border-neo-yellow rounded-md text-sm"
+            />
+          )}
+          {!isInTeamTwo && !teamTwoComplete && (
+            <Button
+              text="+ Team Two"
+              onClick={() => onAddToTeam(fighter, 2)}
+              className="hover:text-neo-yellow p-0 border border-neo-white hover:border-neo-yellow rounded-md text-sm"
+            />
+          )}
+        </div>
       </div>
     </div>
   );
